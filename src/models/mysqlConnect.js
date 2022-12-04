045b9709946e0546229b685baf2d74947ea351b1
João Gabriel Schwabe <schwabejoao@gmail.com>
2022-12-04 20:54:20 -0300

    finished

--- src/models/mysqlConnect.js
+++ src/models/mysqlConnect.js
@@ -2,13 +2,8 @@ const mysql = require("mysql2/promise");
 async function connect() {
     if (global.connection && global.connection.state !== "disconnected")
         return global.connection;
-    const connection = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT,
-    });
+
+    const connection = await mysql.createConnection(process.env.DB_URL);
     console.log("Conectou no MySQL!");
     global.connection = connection;
     return connection;
