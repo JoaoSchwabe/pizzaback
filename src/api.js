460674f8abaeb7d78349ebae4c09573ad599b520
João Gabriel Schwabe <schwabejoao@gmail.com>
2022-12-04 19:01:25 -0300

    finished

--- src/api.js
+++ src/api.js
@@ -3,7 +3,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+
+    app.use(cors());
+    next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 /** ---------ROTAS------*/
