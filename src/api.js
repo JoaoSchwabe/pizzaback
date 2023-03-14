e67efdec359beef74bd5d616fe109699ec79192b
João Gabriel Schwabe <81689363+joaoschwabe@users.noreply.github.com>
2023-03-14 20:59:07 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -14,7 +14,7 @@ app.use(
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
